c6831bc643460f91bda56d118e3948c835211000
Nazir Nayal <37051445+NazirNayal8@users.noreply.github.com>
2019-10-08 15:04:37 +0300

    Add codes

---
+++ Week1/Example.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    string name, surname;
+    
+    cin >> name >> surname;
+
+    cout << "Hello " << name << " " << surname;
+
+}
